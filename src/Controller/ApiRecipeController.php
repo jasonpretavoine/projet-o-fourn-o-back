07e1e7a4c248fddd33f22dec7eb4d2dc588d7006
tricoire arnaud <arnaud.tricoire@hotmail.com>
2024-02-09 14:24:04 +0100

    test

---
+++ src/Controller/ApiRecipeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiRecipeController extends AbstractController
+{
+    /**
+     * Renvoi de la liste de toutes les recettes
+     *
+     * @param RecipeRepository $recipeRepository
+     * @return JsonResponse
+     * 
+     * @Route("/api/recipes", name="api_recipes_get", methods={"GET"})
+     */
+    public function getCollection(RecipeRepository $recipeRepository): JsonResponse
+    {
+        $recipes = $recipeRepository->findAll();
+        return $this->json($recipes, 200, []);
+    }
+
+     
+      /**
+     * Affiche les films par ordre de sortie décroissant
+     * 
+     * @return Response 
+     */
+    #[Route('/api/recipes/view', name: 'app_api_recipes_view', methods: ['GET'])]
+    public function viewRecipes(): Response
+    {
+        return $this->render('api_recipe/index.html.twig');
+    }
+}
