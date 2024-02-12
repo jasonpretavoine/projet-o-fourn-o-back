c6fa3dcf80d28ef7683b53b677528acbf1fa5eab
tricoire arnaud <arnaud.tricoire@hotmail.com>
2024-02-12 14:19:58 +0100

    users bak office ok

---
+++ src/Controller/Back/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/admin/users', name: 'admin_users')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/admin/users/new', name: 'admin_users_new')]
+    public function new(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/user/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/users/{id}/edit', name: 'admin_users_edit')]
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/users/{id}', name: 'admin_users_view', methods: ['GET'])]
+    public function view(User $user): Response
+    {
+        return $this->render('admin/user/view.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/admin/users/{id}', name: 'admin_users_delete', methods: ['POST'])]
+    public function delete(User $user): Response
+    {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('admin_users');
+    }
+}
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -18,28 +18,42 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('picture')
-            ->add('description')
-            ->add('instructions')
+            ->add('name', null, [
+                'label' => 'Nom',
+            ])
+            ->add('picture', null, [
+                'label' => 'Image',
+            ])
+            ->add('description', null, [
+                'label' => 'Description',
+            ])
+            ->add('instructions', null, [
+                'label' => 'Instructions',
+            ])
             ->add('difficulty', ChoiceType::class, [
+                'label' => 'Difficulté',
                 'choices' => [
                     'Facile' => 'facile',
                     'Moyen' => 'moyen',
                     'Difficile' => 'difficile',
                 ],
             ])
-            ->add('preparationTime')
-            ->add('cookingTime')
+            ->add('preparationTime', null, [
+                'label' => 'Temps de préparation',
+            ])
+            ->add('cookingTime', null, [
+                'label' => 'Temps de cuisson',
+            ])
             ->add('servings', ChoiceType::class, [
                 'label' => 'Nombre de personnes',
                 'choices' => array_combine(range(1, 10), range(1, 10)),
             ])
-             
             ->add('rating', ChoiceType::class, [
+                'label' => 'Note',
                 'choices' => array_combine(range(1, 5), range(1, 5))
             ])
             ->add('status', ChoiceType::class, [
+                'label' => 'Statut',
                 'choices' => [
                     'Validé' => '0',
                     'Rejeté' => '1',
@@ -48,22 +62,26 @@ class RecipeType extends AbstractType
             ])
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
-'choice_label' => 'name',
-'multiple' => true,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'label' => 'Catégories',
             ])
             ->add('ustensils', EntityType::class, [
                 'class' => Ustensil::class,
-'choice_label' => 'name',
-'multiple' => true,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'label' => 'Ustensiles',
             ])
             ->add('ingredients', EntityType::class, [
                 'class' => Ingredient::class,
-'choice_label' => 'name',
-'multiple' => true,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'label' => 'Ingrédients',
             ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
-'choice_label' => 'pseudo',
+                'choice_label' => 'pseudo',
+                'label' => 'Utilisateur',
             ])
         ;
     }
