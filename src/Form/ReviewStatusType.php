6077def7d2bbaf885d5e7aad07b2cb5aa3d4f5fd
tricoire arnaud <arnaud.tricoire@hotmail.com>
2024-02-21 15:12:50 +0100

    modif twig

--- src/Form/ReviewStatusType.php
+++ src/Form/ReviewStatusType.php
@@ -4,20 +4,12 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ReviewStatusType extends AbstractType
 {
-    private CsrfTokenManagerInterface $csrfTokenManager;
-
-    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
-    {
-        $this->csrfTokenManager = $csrfTokenManager;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -29,8 +21,7 @@ class ReviewStatusType extends AbstractType
                     'Approuvé' => '1',
                     'Rejeté' => '0',
                 ],
-            ])
-            ->getForm();
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -38,15 +29,5 @@ class ReviewStatusType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Review::class,
         ]);
-    }
-
-    public function getBlockPrefix(): string
-    {
-        return ''; // Doit retourner une chaîne vide pour éviter la surcharge du nom du formulaire dans le nom du champ CSRF
-    }
-
-    public function getCsrfToken(): string
-    {
-        return $this->csrfTokenManager->getToken('review_status')->getValue();
     }
 }
