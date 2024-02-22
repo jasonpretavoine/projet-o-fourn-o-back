f821e2e12a430811003e5afe5d925c13c1e021b5
tricoire arnaud <arnaud.tricoire@hotmail.com>
2024-02-22 10:36:21 +0100

    modif form recipe

--- src/Controller/Back/RecipeController.php
+++ src/Controller/Back/RecipeController.php
@@ -6,6 +6,8 @@ use App\Entity\Recipe;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use App\Repository\ReviewRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\UstensilRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,7 +23,6 @@ class RecipeController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    // Affiche la liste des recettes dans l'interface d'administration
     #[Route('/admin/recipes', name: 'admin_recipes')]
     public function index(RecipeRepository $recipeRepository): Response
     {
@@ -31,15 +32,26 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    // Affiche le formulaire de création d'une nouvelle recette dans l'interface d'administration
     #[Route('/admin/recipes/new', name: 'admin_recipes_new')]
-    public function new(Request $request): Response
+    public function new(Request $request, IngredientRepository $ingredientRepository, UstensilRepository $ustensilRepository): Response
     {
         $recipe = new Recipe();
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer les données des ingrédients et des ustensiles sélectionnés dans le formulaire
+            $ingredients = $form->get('ingredients')->getData();
+            $ustensils = $form->get('ustensils')->getData();
+
+            // Associer les ingrédients et les ustensiles à la recette
+            foreach ($ingredients as $ingredient) {
+                $recipe->addIngredient($ingredient);
+            }
+            foreach ($ustensils as $ustensil) {
+                $recipe->addUstensil($ustensil);
+            }
+
             $this->entityManager->persist($recipe);
             $this->entityManager->flush();
 
@@ -51,14 +63,35 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    // Affiche le formulaire d'édition d'une recette dans l'interface d'administration
     #[Route('/admin/recipes/{id}/edit', name: 'admin_recipes_edit')]
-    public function edit(Request $request, Recipe $recipe): Response
+    public function edit(Request $request, Recipe $recipe, IngredientRepository $ingredientRepository, UstensilRepository $ustensilRepository): Response
     {
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            
+            // Récupérer les données des ingrédients et des ustensiles sélectionnés dans le formulaire
+            $ingredients = $form->get('ingredients')->getData();
+            $ustensils = $form->get('ustensils')->getData();
+
+            // Supprimer les anciens ingrédients et ustensiles associés à la recette
+            foreach ($recipe->getIngredients() as $ingredient) {
+                $recipe->removeIngredient($ingredient);
+            }
+            foreach ($recipe->getUstensils() as $ustensil) {
+                $recipe->removeUstensil($ustensil);
+            }
+
+            // Associer les nouveaux ingrédients et ustensiles à la recette
+            foreach ($ingredients as $ingredient) {
+                $recipe->addIngredient($ingredient);
+            }
+            foreach ($ustensils as $ustensil) {
+                $recipe->addUstensil($ustensil);
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('admin_recipes');
@@ -70,7 +103,6 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    // Affiche les détails d'une recette dans l'interface d'administration
     #[Route('/admin/recipes/{id}', name: 'admin_recipes_view', methods: ['GET'])]
     public function view(Recipe $recipe): Response
     {
@@ -79,7 +111,6 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    // Supprime une recette dans l'interface d'administration
     #[Route('/admin/recipes/{id}/delete', name: 'admin_recipes_delete', methods: ['POST'])]
     public function delete(Recipe $recipe): Response
     {
@@ -92,7 +123,6 @@ class RecipeController extends AbstractController
     #[Route('/admin/recipes/{id}/reviews', name: 'admin_recipes_reviews', methods: ['GET'])]
     public function showReviews(Recipe $recipe, ReviewRepository $reviewRepository): Response
     {
-        // Récupérer les commentaires de la recette spécifique
         $reviews = $reviewRepository->findBy(['recipe' => $recipe]);
 
         return $this->render('admin/recipe/reviews.html.twig', [
