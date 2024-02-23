59ac9ea26a5a3800d1ac987f8a801d33329f81b9
SebastienMan <sebastien.manach@oclock.school>
2024-02-23 15:11:43 +0100

    chore(twig) maj twig v4

--- migrations/Version20240223124922.php
+++ migrations/Version20240223124922.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240222081933 extends AbstractMigration
+final class Version20240223124922 extends AbstractMigration
 {
     public function getDescription(): string
     {
